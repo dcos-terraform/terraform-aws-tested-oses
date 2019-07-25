5dcd70bc03b316d52bac638ad2b16a46451b69c1
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-07-25 10:35:42 +0200

    removed prereq scripts

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 /**
  * AWS Tested OSes
  * ============
- * This module returns for an given OS the AMI aswell as its default user and the prerequisits script
+ * This module returns for an given OS the AMI aswell as its default user
  *
  *
  * EXAMPLE
@@ -17,10 +17,6 @@
 provider "aws" {}
 
 data "aws_region" "current" {}
-
-data "template_file" "os-setup" {
-  template = "${file("${path.module}/platform/cloud/${var.provider}/${var.os}/setup.sh")}"
-}
 
 data "template_file" "aws_ami" {
   template = "$${aws_ami_result}"
--- outputs.tf
+++ outputs.tf
@@ -7,8 +7,3 @@ output "aws_ami" {
   description = "AMI that will be used for the instances instead of the Mesosphere chosen default images. Custom AMIs must fulfill the Mesosphere DC/OS system-requirements: See https://docs.mesosphere.com/1.12/installing/production/system-requirements/"
   value       = "${data.template_file.aws_ami.rendered}"
 }
-
-output "os-setup" {
-  description = "os-setup"
-  value       = "${data.template_file.os-setup.rendered}"
-}
